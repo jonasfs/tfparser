e67f4e08ccebb5473a74c3179727fd61300f00e2
Jonas Ferreira <jonasfscc@gmail.com>
2022-01-23 10:42:30 +0100

    Add missing stuff from initial commit

---
+++ profile/profile_test.go
@@ -0,0 +1,91 @@
+package profile
+
+import (
+	"errors"
+	"testing"
+
+	"topfrag.org/tfparser/database"
+	"topfrag.org/tfparser/models"
+)
+
+var settingsExistsMock func() (*models.Settings, error)
+
+type dbCheckMock struct{}
+
+func (d dbCheckMock) Settings() (*models.Settings, error) {
+	return settingsExistsMock()
+}
+
+func (d dbCheckMock) InitSettings() *models.Settings {
+	return database.DB.InitSettings()
+}
+
+func (d dbCheckMock) SaveSettings(settings *models.Settings) bool {
+	return database.DB.SaveSettings(settings)
+}
+
+func TestCheckFirstTime(t *testing.T) {
+	dbMock := dbCheckMock{}
+	var check bool
+
+	// expect true when settings were not found
+	settingsExistsMock = func() (*models.Settings, error) {
+		err := errors.New("test")
+		return nil, err
+	}
+	check = CheckFirstTime(dbMock)
+	if check != true {
+		t.Error("Expected true, got ", check)
+	}
+
+	// expect true when settings were found
+	// but every attribute is default
+	settingsExistsMock = func() (*models.Settings, error) {
+		var testSettings models.Settings
+		return &testSettings, nil
+	}
+	check = CheckFirstTime(dbMock)
+	if check != true {
+		t.Error("Expected true, got ", check)
+	}
+
+	// expect true when settings were found
+	// but demo path is not set
+	settingsExistsMock = func() (*models.Settings, error) {
+		var testSettings models.Settings
+		testSettings.DemoPathSet = false
+		testSettings.PlayerSet = true
+		return &testSettings, nil
+	}
+	check = CheckFirstTime(dbMock)
+	if check != true {
+		t.Error("Expected true, got ", check)
+	}
+
+	// expect true when settings were found
+	// but player is not set
+	settingsExistsMock = func() (*models.Settings, error) {
+		var testSettings models.Settings
+		testSettings.DemoPathSet = true
+		testSettings.PlayerSet = false
+		return &testSettings, nil
+	}
+	check = CheckFirstTime(dbMock)
+	if check != true {
+		t.Error("Expected true, got ", check)
+	}
+
+	// expect false when settings were found
+	// and demo path and player are set
+	settingsExistsMock = func() (*models.Settings, error) {
+		var testSettings models.Settings
+		testSettings.DemoPathSet = true
+		testSettings.PlayerSet = true
+		return &testSettings, nil
+	}
+	check = CheckFirstTime(dbMock)
+	if check != false {
+		t.Error("Expected false, got ", check)
+	}
+
+}
